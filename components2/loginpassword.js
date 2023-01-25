f14ec38dc3543dcbe0aa6dab16749f7e015f1367
Firoz-PS <ce19b048@smail.iitm.ac.in>
2023-01-25 12:10:11 +0530

    changes in the instruction page

---
+++ components2/loginpassword.js
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import {Form,Button,Container, Modal, Alert} from 'react-bootstrap'
+import { ScrollArea } from '@mantine/core';
+
+// This component checks the password entered by the user is matching with the password stored in env file and if it matches then it redirects to the signup page
+const LoginPassword = () =>{
+    const [password,setPassword]=useState('')
+    const [ischecked,setIschecked]=useState(false)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const validatePassword = () => {
+        if (password === process.env.NEXT_PUBLIC_PASSWORD) {
+            window.location.href = '/signup';
+        }
+        else {
+            Alert('Please enter the correct password');
+        }
+    }
+
+    return(
+        <ScrollArea style={{ height: 600 }}>
+        <div className='flex flex-col p-4'>
+            <div className='text-2xl font-bold mb-3'>
+                Dear Friends, <br/><br/>
+                A warm hello from the Indian Institute of Technology Madras! <br/><br/>
+                We are a team of researchers from the Department of Management Studies, IITM invested in understanding how to improve the quality and accuracy of information shared on social media platforms. Considering the role of social media in information dissemination, we feel it is important to understand how users' experiences with regard to ‘information’ can be enhanced on social media platforms. We invite your valuable contribution to this study about health information on social media. <br/><br/>
+                The study has two phases: <br/>
+                • In the first phase, you have a small information game (10 mins) <br/>
+                • In the second phase,  you are given a questionnaire ( 15 mins) <br/><br/>
+                The participation in this study is voluntary and poses no risk. You may choose to withdraw from the study at any point without giving a reason. Should you have any queries, you may ask the corresponding researcher. <br/><br/>
+                We appreciate your time and contribution to academia. <br/><br/>
+                Best Regards, <br/>
+                Muhammed Sadiq T  (Ph.D. Candidate and Corresponding Researcher), connectsadiqt@gmail.com <br/>
+                Dr. Saji Mathew (Associate Professor, IIT Madras)
+            </div>
+            <div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label={<div> I have read the <span> <Button variant="link" onClick={handleShow}>Consent of Participation</Button></span> and agree to participate in the study.</div>} onChange={(e)=>setIschecked(e.target.checked)}/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                    </Form.Group>
+                    <Button variant="primary" type="button" onClick={validatePassword} disabled={!ischecked}>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title> Consent of Participation </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                • I confirm that I have read and understood the above participant information sheet for the study. <br/><br/>
+                • I understand that my participation in the study is voluntary and that I am free to withdraw at any time, without giving any reason, and without my legal rights being affected. <br/><br/>
+                • I agree not to restrict the use of any data or results that arise from this study, provided such use is only for academic purposes.                
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+        </ScrollArea>
+    )
+}
+
+export default LoginPassword
--- components2/signup.js
+++ components2/signup.js
@@ -12,15 +12,59 @@ const Signup = () =>{
     const [family,setFamily]=useState('')
     const [friend,setFriend]=useState('')
     const [colleague,setColleague]=useState('')
-    const [submittext,setSubmittext]=useState('Submit')
     
-    const [ischecked,setIschecked]=useState(false)
+    const validateDataFields = () => {
+        if (name === '' || email === '' || age === '' || family === '' || friend === '' || colleague === '') {
+            alert('Please fill all the fields');
+            return false;
+        }
+        else if (isNaN(age)) {
+            alert('Please enter a valid age');
+            return false;
+        }
+        else if (age < 18) {
+            alert('You are not eligible to participate in this survey');
+            return false;
+        }
+        else if (age > 90) {
+            alert('Not a valid age');
+            return false;
+        }
+        else if (!email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+            alert('Please enter a valid email address');
+            return false;
+        }
+        else if (!name.match(/^[a-zA-Z ]+$/)) {
+            alert('Please enter a valid name');
+            return false;
+        }
+        else if (!family.match(/^[a-zA-Z ]+$/)) {
+            alert('Please enter a valid family member name');
+            return false;
+        }
+        else if (!friend.match(/^[a-zA-Z ]+$/)) {
+            alert('Please enter a valid friend name');
+            return false;
+        }
+        else if (!colleague.match(/^[a-zA-Z ]+$/)) {
+            alert('Please enter a valid colleague name');
+            return false;
+        }
+        else if (family === friend || family === colleague || friend === colleague) {
+            alert('Please enter different names');
+            return false;
+        }
+        return true;
+    };
+
 
     const submitHandler=async(e)=>{
-        setSubmittext('Loading...........\nPlease Wait..........')
         e.preventDefault();
-        let rt = Math.floor(Math.random() * 4).toString();
-        rt='222'
+        let ratingtype = Math.floor(Math.random() * 4);
+
+        if (validateDataFields() === false) {
+            return;
+        }
         
         axios.post('/api/signupsheet',{
             email:email,
@@ -28,27 +72,25 @@ const Signup = () =>{
             age:age,
             family:family,
             friend:friend,
-            colleague:colleague,
-            rt:rt
+            colleague:colleague
             })
         .then((resp)=>{
             const id=resp.data
             const starttime=new Date().getTime()
-            localStorage.setItem('user',JSON.stringify({id,name,email,age,rt,starttime,family,friend,colleague}))
+            localStorage.setItem('user',JSON.stringify({id,name,email,age,ratingtype,starttime,family,friend,colleague}))
             window.location.href = "/mainpage";  
         })
         .catch((err)=>{
             console.log(e);
-            alert("Invalid signup");
+            alert("Some error occured");
             setName("");
             setAge("");
             setEmail("");
         })
-        setSubmittext('Submit')
         };
       
     return(
-        <ScrollArea style={{ height: 500 }}>
+        <ScrollArea style={{ height: 600 }}>
             <Container>
         <Form className={styles.form} onSubmit={submitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -101,23 +143,12 @@ const Signup = () =>{
                 <Form.Control type="name" placeholder="Colleague" value={colleague} onChange={(e)=>setColleague(e.target.value)}/>
                 </div>
                 <Form.Text className={styles.textmuted}>
-                A Colleagues yo frequently chat with
+                A Colleagues you frequently chat with
                 </Form.Text>        
             </Form.Group>
-
-            
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="I read the instructions and agree to all the terms and conditions" onChange={(e) => setIschecked(e.currentTarget.checked)}/>
-            </Form.Group>
-            {ischecked?
-                <Button variant="primary" type="submit" onClick={()=>setSubmittext('Loading...........\nPlease Wait..........')}>
-                    {submittext}
+                <Button variant="primary" type="submit" >
+                    Submit
                 </Button>
-                :
-                <Button variant="primary" type="submit" disabled>
-                    {submittext}
-                </Button>
-            }
         </Form>
         </Container>
         </ScrollArea>
