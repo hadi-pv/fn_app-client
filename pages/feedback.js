dbdd14d64c2ebd1a19729d48d79192c16fbb40ec
Firoz-PS <ce19b048@smail.iitm.ac.in>
2023-01-26 22:20:01 +0530

    resolved few errors in iframe

--- pages/feedback.js
+++ pages/feedback.js
@@ -5,7 +5,7 @@ export default function Home() {
           <img src='/iitmlogo.png' className='w-[6vh] h-[6vh] md:w-[18vh] md:h-[18vh] absolute top-[1vh] left-[1vh] md:top-[3vh] md:left-[3vh]' />
         </div>
         <div className='absolute top-[8vh] left-[50vw] md:top-[5vh] translate-x-[-50%] flex-col z-2 w-[100vw] h-[90vh] md:w-[50vw] bg-white shadow-2xl p-2 rounded-lg'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSde184ykD6hKNKw6iTnoR5cqsiB2xeId7gUSVVtAEZHh5J28Q/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSde184ykD6hKNKw6iTnoR5cqsiB2xeId7gUSVVtAEZHh5J28Q/viewform?embedded=true" width="100%" height="100%">Loading…</iframe>
         </div>
       </div>
   )
